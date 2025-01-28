dbfc6d3274dd1fde68d9937cadffb1d1b3e4906e
Connor Henning <connorxhenning@gmail.com>
2025-01-28 08:05:33 -0500

    Updated comments

--- ConnorHenning/Lab2/lab2q10.c
+++ ConnorHenning/Lab2/lab2q10.c
@@ -1,31 +1,35 @@
-//10.	[10 Points] For this question you are to design and implement an extension to the circuit you implemented in question 9 above.  For this question, you are to drive an LED circuit similar to that shown in lecture 5 slide 13.  In your circuit you must drive a Green LED using GPIO0, a Red LED using GPIO1, a Blue LED using GPIO2, a Yellow LED using GPIO3, and a White LED using GPIO4.  Your program must scan from GPIO0 to GPIO4, turning each LED on for 200ms before turning that LED off and turning the next LED on.  Once the last LED (GPIO4) has been lit for 200ms, all LEDs must remain dark for 1 second.  Once 1 second has elapsed, the process must begin again at GPIO0, so that there is a pattern of lights that appears to move from GPIO0 to GPIO4, stops for a moment, and starts again at GPIO0. 
- // The program starts off with the inclusion of libraries referenced for functions and pin positions. 
+// The program starts off with the inclusion of libraries referenced for functions and pin positions. 
 #include "pico/stdlib.h"
+
 // The main function that starts the program and is responsible for toggling the LED.
 int main() {
-		// Initializes stdio types that are linked and a counter int 
-stdio_init_all();
+	
+	// Initializes stdio types that are linked and a counter int.
+	stdio_init_all();
 	int i;
+	
+	// A for loop that initializes all output pins.
     for (i = 0; i <= 4; i++) {
 	gpio_init(i);
 	gpio_set_dir(i, GPIO_OUT);
 	}
 	gpio_init(0);
 	gpio_set_dir(0, GPIO_OUT);
-// If there was no issue in initialization the program will repeat a while loop that is forever true which is responsible for indefinitely blinking the LED at varying intervals.
-
+	
+// If there was no issue in initialization the program will repeat a while loop that is forever true which is responsible for indefinitely cycling the LEDs at varying intervals.
 while(true) {
 
 // A loop to cycle and turn pins 2-6 on and then off after waiting (GPIO 0-4)
     for (i = 0; i <= 4; i++) {
-// A function to set the current pin to an on state
-  	gpio_put (i, 1) ;
-// The chip is set to wait (200 ms) of no activity.
-sleep_ms(200);
+		
+		// A function to set the current pin to an on state
+		gpio_put (i, 1) ;
+		// The chip is set to wait (200 ms) of no activity with an LED on.
+		sleep_ms(200);
 		// A function to set the current pin to an off state
-gpio_put (i,0);
-
+		gpio_put (i,0);
     }
+	
 // The chip is set to wait for a full second of no activity.
 sleep_ms(1000);
 
--- ConnorHenning/Lab2/lab2q9.c
+++ ConnorHenning/Lab2/lab2q9.c
@@ -7,39 +7,39 @@ int main() {
 	stdio_init_all();
 	int i;
 
-	// Sets and initializes the pin as an output.
+	// Sets and initializes the pin as a constant output.
 	const int IO_PIN = 22;
 	gpio_init(IO_PIN);
 	gpio_set_dir(IO_PIN, GPIO_OUT);
 	
 	// If there was no issue in initialization the program will repeat a while loop that is forever true which is responsible for indefinitely blinking the LED at varying intervals. 
-
 	while(true) {
 
 		// A simple for loop that repeats five times for the LED to blink at 1Hz across a total of 5 seconds.
 		for (i = 1; i <= 5; i++) {
-		// A basic function to set pin 32 (GPIO22) to an on state
-		gpio_put (IO_PIN, 1) ;
-		// The chip is set to wait for half a second (500 ms) of no activity.
-		sleep_ms(500);
-		// A function to set pin 32 (GPIO22) to an off state
-		gpio_put (IO_PIN,0);
-		// The chip is set to wait for half a second (500 ms) of no activity.
-		sleep_ms(500);
-	}
+			
+			// A basic function to set GPIO22 to an on state
+			gpio_put (IO_PIN, 1) ;
+			// The chip is set to wait for half a second (500 ms) of no activity.
+			sleep_ms(500);
+			// A function to set pin GPIO22 to an off state
+			gpio_put (IO_PIN,0);
+			// The chip is set to wait for half a second (500 ms) of no activity.
+			sleep_ms(500);
+		}
 
-	// A for loop that repeats five times for the LED to blink at 5Hz across a total of 1 second.
-
-	for (i = 1; i <= 5; i++) {
-		// A function to set pin 32 (GPIO22) to an on state
-		gpio_put (IO_PIN, 1) ;
-		// The chip is set to wait for half a second (500 ms) of no activity.
-		sleep_ms(100);
-		// A function to set pin 32 (GPIO22) to an off state
-		gpio_put (IO_PIN,0);
-		// The chip is set to wait for half a second (500 ms) of no activity.
-		sleep_ms(100);
-	}
+		// A for loop that repeats five times for the LED to blink at 5Hz across a total of 1 second.
+		for (i = 1; i <= 5; i++) {
+		
+			// A function to set GPIO22 to an on state
+			gpio_put (IO_PIN, 1) ;
+			// The chip is set to wait for a tenth of a second (100 ms) of no activity.
+			sleep_ms(100);
+			// A function to set GPIO22 to an off state
+			gpio_put (IO_PIN,0);
+			// The chip is set to wait for a tenth of a second (100 ms) of no activity.
+			sleep_ms(100);
+		}
 
 	}
 	return 0;
